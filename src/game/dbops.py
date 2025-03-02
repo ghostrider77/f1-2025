b892542bdb38ca67ca81c84b5e383a2c55286aa7
Tamas Kurics <tamas.kurics@gmail.com>
2025-03-02 06:39:48 +0100

    feat: add create user functionality
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/game/dbops.py
+++ src/game/dbops.py
@@ -1,9 +1,15 @@
 import functools as ft
 
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from typing import Callable, Concatenate, ParamSpec, TypeVar
 
 from ..database.engine import DBEngine
+from ..database.entities import User
+from ..utils.auth import hash_password
+from ..utils.enums import RequestStatus
+from ..web import RequestResponse
+from ..web.user.models import UserModel
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -22,3 +28,20 @@ class DBOperations:
                 return func(self, session, *args, **kwargs)
 
         return wrapper
+
+    @_with_engine
+    def create_user(self, session: Session, registration: UserModel) -> RequestResponse:
+        if (hashed_password := hash_password(registration.password)) is None:
+            error_msg = "Password should be an ASCII-string with length between 8 and 256."
+            return RequestResponse(status=RequestStatus.FAILURE, message=error_msg)
+
+        user_entry = User(username=registration.username, password=hashed_password)
+        try:
+            session.add(user_entry)
+            session.commit()
+            return RequestResponse(status=RequestStatus.SUCCESS)
+
+        except IntegrityError:
+            session.rollback()
+            error_msg = f"Could not create User({registration.username}), it is already registered."
+            return RequestResponse(status=RequestStatus.FAILURE, message=error_msg)
