1e5d73a3a4f218639d8efc117e1cefb2bbadea50
Tamas Kurics <tamas.kurics@gmail.com>
2025-03-03 16:51:07 +0100

    feat: initialize database
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/application.py
+++ src/application.py
@@ -4,16 +4,28 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from typing import AsyncGenerator
 
+from src.web.participants.models import DriverModel
+from . import PACKAGE_DIR
 from .configs.config import SERVICE_CONFIG
 from .database.entities import create_all_tables
-from .game import db_engine
+from .game import db_engine, db_ops
+from .utils.fileio import read_json_file
 from .web.participants.router import router as actor_router
 from .web.user.router import router as user_router
+
+
+def initialize_database() -> None:
+    for constructor in read_json_file(PACKAGE_DIR / "configs/data/constructors.json"):
+        db_ops.create_constructor(constructor)
+
+    for line in read_json_file(PACKAGE_DIR / "configs/data/drivers.json"):
+        db_ops.create_driver(DriverModel.model_validate(line))
 
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
     create_all_tables(db_engine.engine())
+    initialize_database()
     yield
 
     db_engine.close()
