203e90965576bb5be2e956677a341738d5c6cc96
Tamas Kurics <tamas.kurics@gmail.com>
2025-03-09 07:47:03 +0100

    feat: add model for making predictions
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/web/participants/models.py
+++ src/web/participants/models.py
@@ -1,5 +1,5 @@
 from datetime import date
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 from pydantic.alias_generators import to_camel
 
 from ...database.enums import RaceFormat
@@ -10,6 +10,26 @@ class DriverModel(BaseModel):
 
     name: str = Field(max_length=128)
     country: str = Field(max_length=64)
+
+
+class PredictionModel(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    username: str
+    password: str
+
+    race_name: str
+    race_format: RaceFormat
+    drivers: list[str]
+
+    @field_validator("drivers", mode="after")
+    @classmethod
+    def check_if_driver_list_is_unique(cls, drivers: list[str]) -> list[str]:
+        unique_drivers = set(drivers)
+        if len(unique_drivers) != len(drivers):
+            raise ValueError("Driver list is not unique!")
+
+        return drivers
 
 
 class RaceModel(BaseModel):
